d45a3fbc67fd1dc51b47d9b17bf18365a09f1f12
takegue <takegue@gmail.com>
2022-08-28 10:20:26 +0900

    chore: clean up debug code

--- src/index.ts
+++ src/index.ts
@@ -672,7 +672,6 @@ const buildDAG = async (
           target.bigquery,
           {
             task: new Task(target.bigquery, async () => {
-              console.log(target.file)
               await Promise.all(
                 target.dependencies
                   .map(
@@ -700,11 +699,9 @@ const buildDAG = async (
 
   const tasks = [...DAG.values()]
     .map(({ task }) => {
-      console.log(task.name)
       limit(async () => await task.run());
       return task;
     });
-  console.dir(tasks)
 
   const reporter = new Reporter(tasks);
   for await (let report of reporter.show_until_finished()) {
