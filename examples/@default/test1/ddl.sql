e5847237a42a458312323c46e8da81ca20462dfc
takegue <takegue@gmail.com>
2022-08-28 11:22:09 +0900

    Remove unused

--- examples/@default/test1/ddl.sql
+++
@@ -1,4 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS `project-id-7288898082930342315.test1`
-OPTIONS(
-  location="us"
-);
--- examples/@default/test1/hoge/ddl.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS `project-id-7288898082930342315.test1.hoge`
-(
-  a1 STRING
-);
