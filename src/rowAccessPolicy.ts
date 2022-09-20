47e4b47060269a57f47284f02175fb5dec61d48e
takegue <takegue@gmail.com>
2022-09-21 06:59:14 +0900

    Impl. RowAccessPolicy fetcher

---
+++ src/rowAccessPolicy.ts
@@ -0,0 +1,53 @@
+
+import {
+  BigQuery,
+} from '@google-cloud/bigquery';
+
+
+type RowAccessPolicy = {
+  rowAccessPolicyReference: {
+    tableId: string,
+    datasetId: string,
+    projectId: string,
+    policyId: string,
+  },
+  filterPredicate: string,
+  creationTime: string,
+  lastModifiedTime: string,
+}
+
+
+async function fetchRowAccessPolicy(client: BigQuery, datasetId: string, tableId: string) {
+  let ret: RowAccessPolicy[][] = [];
+  await new Promise((resolve, reject) => {
+    client.request({
+      method: 'GET',
+      uri: `/datasets/${datasetId}/tables/${tableId}/rowAccessPolicies`
+    },
+      (err, resp) => {
+        console.log('called2')
+        if (err) {
+          console.error(err);
+          reject(err)
+          return
+        }
+        let nextQuery = null;
+        if (resp.nextPageToken) {
+          nextQuery = Object.assign({}, {
+            pageToken: resp.nextPageToken,
+          });
+          console.dir(nextQuery)
+        }
+
+        ret.push(resp.rowAccessPolicies)
+        resolve(ret)
+      }
+    )
+  })
+  return ret.flat()
+}
+
+export {
+  RowAccessPolicy,
+  fetchRowAccessPolicy,
+}
