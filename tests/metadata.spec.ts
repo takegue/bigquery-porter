d49c5e3e5ab75844966f4e89be7e4ad2a71eef29
takegue <takegue@gmail.com>
2022-09-28 05:58:05 +0900

    Add test case for Push

--- tests/metadata.spec.ts
+++ tests/metadata.spec.ts
@@ -18,18 +18,9 @@ describe('syncMetadata: Pull', () => {
   const bqClient = new BigQuery();
   const _dataset = fs.mkdtempSync(`${tmpdir()}${path.sep}`);
   const _resource = 'resource';
-
-  beforeAll(async () => {
-    const [dataset] = await bqClient.createDataset(path.basename(_dataset), {});
-    // https://cloud.google.com/bigquery/docs/reference/v2/tables#resource
-    dataset.createTable('sample_table', {
-      schema: 'id:integer, name:string',
-      description: 'sample table',
-    });
-  });
+  const dPath = path.join(_dataset, _resource);
 
   afterAll(async () => {
-    bqClient.dataset(path.basename(_dataset)).delete({ force: true });
     fs.rmdirSync(_dataset, { recursive: true });
   });
 
@@ -49,8 +40,6 @@ describe('syncMetadata: Pull', () => {
     );
 
     // test
-    const dPath = path.join(_dataset, _resource);
-    console.log(dPath);
     await syncMetadata(table, dPath);
 
     const _load = (f: string) => fs.readFileSync(path.join(dPath, f), 'utf-8');
@@ -72,14 +61,84 @@ describe('syncMetadata: Pull', () => {
       'int',
     );
 
-    // test
-    const dPath = path.join(_dataset, _resource);
-    console.log(dPath);
     await syncMetadata(table, dPath);
 
     const _load = (f: string) => fs.readFileSync(path.join(dPath, f), 'utf-8');
     expect(fs.existsSync(path.join(dPath, 'metadata.json'))).toBe(true);
     expect(JSON.parse(_load('metadata.json')))
       .toMatchSnapshot();
+
+    expect(fs.existsSync(path.join(dPath, 'schema.json'))).toBe(false);
+
+    expect(fs.existsSync(path.join(dPath, 'README.md'))).toBe(false);
+  });
+});
+
+describe('syncMetadata: Push', () => {
+  const bqClient = new BigQuery();
+  const _local = fs.mkdtempSync(`${tmpdir()}${path.sep}`);
+
+  const _dataset = path.basename(_local);
+  const _fsResource = _local;
+
+  // Utils
+  const toPath = (p: string) => path.join(_fsResource, p);
+  const _load = (f: string) =>
+    fs.readFileSync(path.join(_fsResource, f), 'utf-8');
+  const _write = (p: string, content: string) =>
+    fs.promises.writeFile(
+      path.join(_fsResource, p),
+      content,
+      'utf-8',
+    );
+
+  beforeAll(async () => {
+    const [dataset] = await bqClient.createDataset(path.basename(_dataset), {});
+    // https://cloud.google.com/bigquery/docs/reference/v2/tables#resource
+    await dataset.createTable('sample_table', {
+      schema: 'id:integer, name:string',
+      description: 'sample table',
+    });
+  });
+
+  afterAll(async () => {
+    bqClient.dataset(_dataset).delete({ force: true });
+    fs.rmdirSync(_local, { recursive: true });
+  });
+
+  it('for Table: sample_table', async () => {
+    const table = bqClient.dataset(_dataset).table(
+      'sample_table',
+    );
+    const afterDescription = 'Updated description';
+    const afterSchema = [
+      {
+        name: 'id',
+        type: 'INTEGER',
+        description: 'Updated description for \'id\'',
+      },
+      {
+        name: 'name',
+        type: 'STRING',
+      },
+    ];
+
+    // test
+    await Promise.all([
+      _write('README.md', afterDescription),
+      _write('schema.json', JSON.stringify(afterSchema)),
+    ]);
+
+    await syncMetadata(table, _fsResource, { push: true });
+    const [metadata] = await table.getMetadata();
+
+    expect(fs.existsSync(toPath('README.md'))).toBe(true);
+    expect(_load('README.md'))
+      .toBe(afterDescription);
+
+    expect(fs.existsSync(toPath('schema.json'))).toBe(true);
+    expect(JSON.parse(_load('schema.json')))
+      .toMatchSnapshot();
+    expect(metadata.schema.fields).toBe(afterSchema);
   });
 });
