7770a8882a9620d0ed8bd4877a14ed6b9846f450
takegue <takegue@gmail.com>
2022-12-11 09:31:34 +0900

    Alleviate timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
   test: {
     // FIXME: Couldn't thread in linux due to "Module did not self-register Error"
     threads: false,
-    testTimeout: 3000,
+    testTimeout: 5000,
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'json', 'html'],
