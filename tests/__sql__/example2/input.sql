a2c17a48f029a7fc684fda22643064ff0f0c26c6
takegue <takegue@gmail.com>
2022-12-25 14:11:08 +0900

    Add format rule: reference identifier replacement partially matched to namespace (#58)
    Resolve #57

--- tests/__sql__/example2/input.sql
+++ tests/__sql__/example2/input.sql
@@ -1,74 +1,12 @@
-create or replace function `v0.zgensql__partition_alignment2`(
-  destination STRUCT<project_id STRING, dataset_id STRING, table_id STRING>
-  , sources ARRAY<STRUCT<project_id STRING, dataset_id STRING, table_id STRING>>
-  , partition_alignments ARRAY<STRUCT<destination STRING, sources ARRAY<STRING>>>
-)
-as
-((
-  with
-    sql__information_schema as (
-      select as value
-          string_agg(
-            format("""
-              select
-                '%s' as label
-                , '%s' as argument
-                , *
-              from `%s.INFORMATION_SCHEMA.PARTITIONS`
-              where %s
-              """
-              , label
-              , target.table_id
-              , coalesce(
-                format('%s.%s', target.project_id, target.dataset_id)
-                , target.dataset_id
-                , error(format('Invalid target: %t', target))
-              )
-              , format(if(
-                contains_substr(target.table_id, '*')
-                , 'starts_with(table_name, replace("%s", "*", ""))'
-                , 'table_name = "%s"'
-                )
-                , target.table_id)
-            )
-            , '\nunion all'
-          )
-      from unnest([
-        struct('destination' as label, destination as target)
-      ] || array(select as struct 'source', s from unnest(sources) s))
-    )
-    , ret as (
-      select
-        *
-      from sql__information_schema
-    )
-
-    select * from ret
-));
-
+-- Unit test
 begin
-  declare query string;
+  -- dataset for test
+  create schema if not exists `tmp_dataset`;
+  call `other_dataset.init`();
 
-  create schema if not exists `zpreview_test__alignment`;
+  -- reference
+  call `other_dataset.awesome_procedure`((null, 'tmp_dataset'));
 
-  create or replace table `zpreview_test__alignment.dest1`
-  partition by date_jst
-  as select date '2006-01-02' as date_jst
-  ;
-
-  create or replace table `zpreview_test__alignment.ref1`
-  partition by date_jst
-  as select date '2006-01-02' as date_jst
-  ;
-
-  set query = `v0.zgensql__partition_alignment`(
-    (string(null), "zpreview_test__alignment", 'dest1')
-    , [
-      (string(null), "zpreview_test__alignment", 'ref1')
-    ]
-    , `v0.alignment__day2day`('2006-01-02', '2006-01-02')
-  );
-
-  execute immediate query;
-  drop schema if exists `zpreview_test__alignment` cascade;
+  -- dataset for test
+  drop schema if exists `tmp_dataset` CASCADE;
 end
--- tests/format.spec.ts
+++ tests/format.spec.ts
@@ -1,65 +1,87 @@
-import { describe, expect, it } from 'vitest';
-import Parser from 'tree-sitter';
-import Language from 'tree-sitter-sql-bigquery';
-import { fixDestinationSQL } from '../src/commands/fix.js';
-import * as fs from 'fs';
+import { describe, expect, it } from "vitest";
+import Parser from "tree-sitter";
+import Language from "tree-sitter-sql-bigquery";
+import { fixDestinationSQL } from "../src/commands/fix.js";
+import * as fs from "fs";
 
-describe('Command test: format command', async () => {
+describe("Command test: format command", async () => {
   const cases: Array<{
-    input: [string, string];
+    input: ["table_or_routine" | "dataset" | "project", string, string];
     expected: string;
   }> = [
-    {
-      input: [
-        'awesome-project.sandbox.hoge',
-        'create table\n`awesome-project.sandbox.wrong_name` as select 1',
-      ],
-      expected: 'create table\n`awesome-project.sandbox.hoge` as select 1',
-    },
-    {
-      input: [
-        'awesome-project.sandbox',
-        'create schema `awesome-project.wrong_name`',
-      ],
-      expected: 'create schema `awesome-project.sandbox`',
-    },
-    {
-      input: [
-        'awesome-project.sandbox.hoge',
-        'create or replace function `sandbox.wrong_name`() as (1)',
-      ],
-      expected:
-        'create or replace function `awesome-project.sandbox.hoge`() as (1)',
-    },
-    {
-      input: [
-        'awesome-project.sandbox.hoge',
-        'create or replace table function awesomeproject.wrong_name() as (select 1)',
-      ],
-      expected:
-        'create or replace table function `awesome-project.sandbox.hoge`() as (select 1)',
-    },
-    {
-      input: [
-        'awesome-project.sandbox.correct_name',
-        `create or replace procedure sandbox.wrong_name(in argument int64)
+      {
+        input: [
+          "table_or_routine",
+          "awesome-project.sandbox.hoge",
+          "create table\n`awesome-project.sandbox.wrong_name` as select 1",
+        ],
+        expected: "create table\n`awesome-project.sandbox.hoge` as select 1",
+      },
+      {
+        input: [
+          "dataset",
+          "awesome-project.sandbox",
+          "create schema `awesome-project.wrong_name`",
+        ],
+        expected: "create schema `awesome-project.sandbox`",
+      },
+      {
+        input: [
+          "table_or_routine",
+          "awesome-project.sandbox.hoge",
+          "create or replace function `sandbox.wrong_name`() as (1)",
+        ],
+        expected:
+          "create or replace function `awesome-project.sandbox.hoge`() as (1)",
+      },
+      {
+        input: [
+          "table_or_routine",
+          "awesome-project.sandbox.hoge",
+          "create or replace table function awesomeproject.wrong_name() as (select 1)",
+        ],
+        expected:
+          "create or replace table function `awesome-project.sandbox.hoge`() as (select 1)",
+      },
+      {
+        input: [
+          "table_or_routine",
+          "awesome-project.sandbox.correct_name",
+          `create or replace procedure sandbox.wrong_name(in argument int64)
             options(description="test")
             begin select 1; end`,
-      ],
-      expected:
-        `create or replace procedure \`awesome-project.sandbox.correct_name\`(in argument int64)
+        ],
+        expected:
+          `create or replace procedure \`awesome-project.sandbox.correct_name\`(in argument int64)
             options(description="test")
             begin select 1; end`,
-    },
-    {
-      input: [
-        'sandbox.correct_name',
-        fs.readFileSync('tests/__sql__/example1/input.sql', 'utf8'),
-      ],
-      expected: fs.readFileSync('tests/__sql__/example1/expected.sql', 'utf8'),
-    },
-  ];
-  it.concurrent.each(cases)('Example SQL %#', async (args) => {
+      },
+      {
+        input: [
+          "table_or_routine",
+          "sandbox.correct_name",
+          fs.readFileSync("tests/__sql__/example1/input.sql", "utf8"),
+        ],
+        expected: fs.readFileSync("tests/__sql__/example1/expected.sql", "utf8"),
+      },
+      {
+        input: [
+          "table_or_routine",
+          "replaced_schema.awesome_procedure",
+          fs.readFileSync("tests/__sql__/example2/input.sql", "utf8"),
+        ],
+        expected: fs.readFileSync("tests/__sql__/example2/expected.sql", "utf8"),
+      },
+      {
+        input: [
+          "table_or_routine",
+          "hogehoeg",
+          fs.readFileSync("tests/__sql__/example2/input.sql", "utf8"),
+        ],
+        expected: fs.readFileSync("tests/__sql__/example2/input.sql", "utf8"),
+      },
+    ];
+  it.concurrent.each(cases)("Example SQL %#", async (args) => {
     const { input, expected } = args;
     const parser = new Parser();
     parser.setLanguage(Language);
