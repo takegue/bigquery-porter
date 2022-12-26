ffff2d37c77959e1f5afef4193e8ab3e11e3a5a5
takegue <takegue@gmail.com>
2022-12-27 06:27:21 +0900

    chore: clean up codes

--- tests/bigquery/rowAcessPolicies.spec.ts
+++ tests/bigquery/rowAcessPolicies.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { BigQuery } from '@google-cloud/bigquery';
 import { fetchRowAccessPolicy } from '../..//src/rowAccessPolicy.js';
@@ -8,26 +8,30 @@ const payload = [{
     projectId: 'example_project',
     datasetId: 'sandbox',
     tableId: 'sample_table',
-    policyId: 'sales_us_filter'
+    policyId: 'sales_us_filter',
   },
   filterPredicate: 'a = 2',
   creationTime: '2022-09-19T23:38:49.575882Z',
-  lastModifiedTime: '2022-09-19T23:38:49.575882Z'
-}]
+  lastModifiedTime: '2022-09-19T23:38:49.575882Z',
+}];
 
 describe('fetchRowAccessPolicy', () => {
   it('fetchRowAccessPolicy should call request', async () => {
-    const bqClient = new BigQuery()
+    const bqClient = new BigQuery();
     // mocking
-    const mock = vi.fn().mockImplementation(bqClient.request)
-    bqClient.request = mock
+    const mock = vi.fn().mockImplementation(bqClient.request);
+    bqClient.request = mock;
     mock.mockImplementationOnce((_, cb) => {
-      cb(null, { nextPageToken: null, rowAccessPolicies: payload })
-      return
-    })
+      cb(null, { nextPageToken: null, rowAccessPolicies: payload });
+      return;
+    });
 
-    const data = await fetchRowAccessPolicy(bqClient, 'sandbox', 'sample_table');
+    const data = await fetchRowAccessPolicy(
+      bqClient,
+      'sandbox',
+      'sample_table',
+    );
     expect(data).toEqual(payload);
-    expect(mock).toHaveBeenCalledTimes(1)
-  })
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
 });
