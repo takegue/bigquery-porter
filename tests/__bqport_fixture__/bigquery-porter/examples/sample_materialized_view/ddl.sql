f98088440a04c60aeaa177ee84c1a361696675b5
takegue <takegue@gmail.com>
2023-03-21 21:22:33 +0900

    Update

--- tests/__bqport_fixture__/bigquery-porter/examples/sample_materialized_view/ddl.sql
+++
@@ -1,2 +0,0 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS  `examples.sample_materialized_view`
-AS select 1 as a from `bigquery-porter.examples.sample_table`;
--- tests/__bqport_fixture__/bigquery-porter/examples/sample_partition_table/ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS `examples_eu`
-OPTIONS(
-  location="eu"
-);
-
-CREATE TABLE IF NOT EXISTS `examples_eu.sample_table`
-AS
-SELECT 1 as a;
